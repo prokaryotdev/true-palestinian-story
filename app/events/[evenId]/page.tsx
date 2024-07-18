4bc98ff539f62be63d41bd97d234debd2b329227
prokaryotdev <cosmiceclipse69@gmail.com>
2024-07-18 03:39:35 -0700

    create event-page

---
+++ app/events/[evenId]/page.tsx
@@ -0,0 +1,12 @@
+import EventHero from "@/components/event-page/event-hero";
+import EventSources from "@/components/event-page/event-sources";
+const page = () => {
+  return (
+    <div>
+      <EventHero />
+      <EventSources />
+    </div>
+  );
+};
+
+export default page;
