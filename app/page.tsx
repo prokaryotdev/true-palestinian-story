0b8eec3237d9fbd2bf4616df3ca8baad9796df89
prokaryotdev <cosmiceclipse69@gmail.com>
2024-07-18 03:36:42 -0700

    add max-width to home

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Hero from "@/components/home/hero";
 import Timeline from "@/components/home/timeline";
 const Home = () => {
   return (
-    <>
+    <div className="mx-auto w-full max-w-[768px]">
       <Hero />
       <Timeline />
-    </>
+    </div>
   );
 };
 
