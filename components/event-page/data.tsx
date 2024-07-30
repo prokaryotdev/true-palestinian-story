fa498188623e0fed84092c34c3788d0c05c713be
Adam <152484347+prokaryotdev@users.noreply.github.com>
2024-07-30 11:48:50 -0700

    Update data.tsx

--- components/event-page/data.tsx
+++ components/event-page/data.tsx
@@ -1,77 +1,100 @@
-const events = [
+const data = [
   {
-    id: "1",
-    label: "The Great Arab Revolt",
-    image: "/img1.png",
-    title: "1936",
-    month: [
-      {
-        month: "January",
-        desc: "Palestinian revolt against British colonial rule and mass Jewish immigration, demanding self-determination.",
-      },
-      {
-        month: "March",
-        desc: "Peel Commission proposes partition of Palestine, rejected by Palestinians as it threatens their territorial integrity.",
-      },
-      {
-        month: "July",
-        desc: "British White Paper limits Jewish immigration and land purchases, aiming to protect Palestinian rights.",
-      },
-    ],
+    id: 1,
+    type: "books",
+    title: "The Ethnic Cleansing of Palestine",
+    author: "Ilan Pappe",
+    year: 2007,
   },
   {
-    id: "2",
-    label: "UN Partition Plan",
-    image: "/img2.png",
-    title: "1947",
-    month: [
-      {
-        month: "November",
-        desc: "UN General Assembly adopts Resolution 181, proposing partition of Palestine into Jewish and Arab states against Palestinian objections.",
-      },
-    ],
+    id: 2,
+    type: "books",
+    title: "The Ethnic Cleansing of Palestine",
+    author: "Ilan Pappe",
+    year: 2007,
   },
   {
-    id: "3",
-    label: "Nakba",
-    image: "/img3.png",
-    title: "1948",
-    month: [
-      {
-        month: "May 14",
-        desc: "Israel declares independence, sparking Arab-Israeli War and displacement of over 700,000 Palestinians.",
-      },
-      {
-        month: "May 15",
-        desc: "Palestinian communities face mass expulsion and loss of homes as Israel gains control of key territories.",
-      },
-    ],
+    id: 3,
+    type: "books",
+    title: "The Ethnic Cleansing of Palestine",
+    author: "Ilan Pappe",
+    year: 2007,
   },
   {
-    id: "4",
-    label: "Six-Day War",
-    image: "/img1.png",
-    title: "1967",
-    month: [
-      {
-        month: "June",
-        desc: "Israel captures Gaza Strip, West Bank, and East Jerusalem, intensifying Palestinian grievances over lost land.",
-      },
-    ],
+    id: 4,
+    type: "books",
+    title: "The Ethnic Cleansing of Palestine",
+    author: "Ilan Pappé",
+    year: 2006,
   },
   {
-    id: "5",
-    label: "First Intifada",
-    image: "/img2.png",
-    title: "1987",
-    month: [
-      {
-        month: "December",
-        desc: "Palestinian uprising begins against Israeli occupation and discriminatory policies.",
-      },
-    ],
+    id: 5,
+    type: "documentary",
+    title: "5 Broken Cameras",
+    description:
+      "A documentary capturing the non-violent resistance of a Palestinian farmer to the actions of the Israeli army. It chronicles a village's resistance to a nearby Israeli settlement.",
+    year: 2011,
   },
-
+  {
+    id: 6,
+    type: "documentary",
+    title: "The Wanted 18",
+    description:
+      "A documentary about the efforts of Palestinians to start a small dairy collective during the First Intifada. It illustrates the challenges faced by ordinary Palestinians in their pursuit of independence and self-sufficiency.",
+    year: 2014,
+  },
+  {
+    id: 7,
+    type: "documentary",
+    title: "Salt of This Sea",
+    description:
+      "A drama film following a Brooklyn-born woman of Palestinian descent who travels to Israel and Palestine to retrieve her grandfather's savings.",
+    year: 2008,
+  },
+  {
+    id: 8,
+    type: "documentary",
+    title: "Occupation 101",
+    description:
+      "An educational documentary about the Israeli-Palestinian conflict, focusing on its history and impact on the Palestinian people.",
+    year: 2006,
+  },
+  {
+    id: 9,
+    type: "articles",
+    title: "Palestinian Refugees and the Right of Return",
+    author: "Nur Masalha",
+    year: 2001,
+    summary:
+      "This article explores the historical and political complexities surrounding the Palestinian refugee issue, emphasizing the right of return as a central tenet of Palestinian identity and justice.",
+  },
+  {
+    id: 10,
+    type: "articles",
+    title: "The Balfour Declaration and its Consequences",
+    author: "Rashid Khalidi",
+    year: 2017,
+    summary:
+      "This article examines the historical impact and ongoing ramifications of the Balfour Declaration on Palestinian rights and the Arab-Israeli conflict.",
+  },
+  {
+    id: 11,
+    type: "articles",
+    title: "The Question of Palestine",
+    author: "Edward Said",
+    year: 1979,
+    summary:
+      "In this seminal work, Edward Said analyzes the history and contemporary politics of Palestine, challenging Western narratives and advocating for Palestinian self-determination.",
+  },
+  {
+    id: 12,
+    type: "articles",
+    title: "Reflections War Against the Palestinians",
+    author: "Noam Chomsky and Ilan Pappé",
+    year: 2010,
+    summary:
+      "This article provides critical analysis of Israel's military operations in Gaza, discussing the humanitarian crisis and international law.",
+  },
 ];
 
-export default events;
+export default data;
